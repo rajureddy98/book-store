a72b1fba4fc17e0742680f6ab36607aa731de29e
rajureddy98 <59072171+rajureddy98@users.noreply.github.com>
2023-03-16 06:00:53 +0530

    Update appConstants.js

--- bookstore-frontend-react-app/src/constants/appConstants.js
+++ bookstore-frontend-react-app/src/constants/appConstants.js
@@ -1,3 +1,3 @@
-export const BACKEND_API_GATEWAY_URL = 'https://api-gateway-rajureddy-konga-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com';
+export const BACKEND_API_GATEWAY_URL = 'api-gateway-service.default.svc.cluster.local';
 export const APP_CLIENT_ID = '93ed453e-b7ac-4192-a6d4-c45fae0d99ac';
 export const APP_CLIENT_SECRET = 'client.devd123';
