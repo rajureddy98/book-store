560c18c7b9b1616267f8ed57a1a46bcf0aaae9f7
rajureddy98 <59072171+rajureddy98@users.noreply.github.com>
2023-06-24 03:33:25 +0530

    Update appConstants.js

--- bookstore-frontend-react-app/src/constants/appConstants.js
+++ bookstore-frontend-react-app/src/constants/appConstants.js
@@ -1,3 +1,3 @@
-export const BACKEND_API_GATEWAY_URL = 'http://api-gateway-service:80';
+export const BACKEND_API_GATEWAY_URL = 'api-gateway-service.book-store.svc.cluster.local';
 export const APP_CLIENT_ID = '93ed453e-b7ac-4192-a6d4-c45fae0d99ac';
 export const APP_CLIENT_SECRET = 'client.devd123';
