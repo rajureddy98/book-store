10faa5d41a29f3b1c9620bb1f4e2782190838261
rajureddy98 <59072171+rajureddy98@users.noreply.github.com>
2023-06-24 02:58:20 +0530

    Update appConstants.js

--- bookstore-frontend-react-app/src/constants/appConstants.js
+++ bookstore-frontend-react-app/src/constants/appConstants.js
@@ -1,3 +1,3 @@
-export const BACKEND_API_GATEWAY_URL = 'http://a97292d07c52749f68d30606f221a4ac-9729278.us-west-1.elb.amazonaws.com';
+export const BACKEND_API_GATEWAY_URL = 'http://api-gateway-service:80/';
 export const APP_CLIENT_ID = '93ed453e-b7ac-4192-a6d4-c45fae0d99ac';
 export const APP_CLIENT_SECRET = 'client.devd123';
